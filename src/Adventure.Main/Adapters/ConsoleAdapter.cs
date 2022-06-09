1522a66295c9b3cd20d4b2608a968bdf8a92e5d6
Brendan Travis <brendantravis1@gmail.com>
2022-06-09 06:29:49 +0100

    Add all methods to ConsoleAdapter

--- src/Adventure.Main/Adapters/ConsoleAdapter.cs
+++ src/Adventure.Main/Adapters/ConsoleAdapter.cs
@@ -8,9 +8,121 @@ internal class ConsoleAdapter : IConsoleAdapter
     public int CursorTop { get => Console.CursorTop; set => Console.CursorTop = value; }
     public ConsoleColor ForegroundColor { get => Console.ForegroundColor; set => Console.ForegroundColor = value; }
 
+    public void WriteLine(string format, object arg0) => Console.WriteLine(format, arg0);
+
+    public void WriteLine(string format, object arg0, object arg1) => Console.WriteLine(format, arg0, arg1);
+
+    public void WriteLine(string format, object arg0, object arg1, object arg2) => Console.WriteLine(format, arg0, arg1, arg2);
+
+    public void WriteLine(string format, object[] arg) => Console.WriteLine(format, arg);
+
+    public void Write(string format, object arg0) => Console.Write(format, arg0);
+
+    public void Write(string format, object arg0, object arg1) => Console.Write(format, arg0, arg1);
+
+    public void Write(string format, object arg0, object arg1, object arg2) => Console.Write(format, arg0, arg1, arg2);
+
+    public void Write(string format, object[] arg) => Console.Write(format, arg);
+
+    public void Write(bool value) => Console.Write(value);
+
+    public void Write(char value) => Console.Write(value);
+
+    public void Write(char[] buffer) => Console.Write(buffer);
+
+    public void Write(char[] buffer, int index, int count) => Console.Write(buffer, index, count);
+
+    public void Write(double value) => Console.Write(value);
+
+    public void Write(decimal value) => Console.Write(value);
+
+    public void Write(float value) => Console.Write(value);
+
+    public void Write(int value) => Console.Write(value);
+
+    public void Write(uint value) => Console.Write(value);
+
+    public void Write(long value) => Console.Write(value);
+
+    public void Write(ulong value) => Console.Write(value);
+
+    public void Write(object value) => Console.Write(value);
+
+    public void Write(string value) => Console.Write(value);
+
+    public ConsoleKeyInfo ReadKey() => Console.ReadKey();
+
     public ConsoleKeyInfo ReadKey(bool intercept) => Console.ReadKey(intercept);
+
     public void ResetColor() => Console.ResetColor();
+
+    public void SetBufferSize(int width, int height) => Console.SetBufferSize(width, height);
+
+    public void SetWindowPosition(int left, int top) => Console.SetWindowPosition(left, top);
+
+    public void SetWindowSize(int width, int height) => Console.SetWindowSize(width, height);
+
+    public ValueTuple<int,int> GetCursorPosition() => Console.GetCursorPosition();
+
+    public void Beep() => Console.Beep();
+
+    public void Beep(int frequency, int duration) => Console.Beep(frequency, duration);
+
+    public void MoveBufferArea(int sourceLeft, int sourceTop, int sourceWidth, int sourceHeight, int targetLeft, int targetTop) => Console.MoveBufferArea(sourceLeft, sourceTop, sourceWidth, sourceHeight, targetLeft, targetTop);
+
+    public void MoveBufferArea(int sourceLeft, int sourceTop, int sourceWidth, int sourceHeight, int targetLeft, int targetTop, char sourceChar, ConsoleColor sourceForeColor, ConsoleColor sourceBackColor) => Console.MoveBufferArea(sourceLeft, sourceTop, sourceWidth, sourceHeight, targetLeft, targetTop, sourceChar, sourceForeColor, sourceBackColor);
+
+    public void Clear() => Console.Clear();
+
     public void SetCursorPosition(int left, int top) => Console.SetCursorPosition(left, top);
-    public void Write(string? value) => Console.Write(value);
+
+    public Stream OpenStandardInput() => Console.OpenStandardInput();
+
+    public Stream OpenStandardInput(int bufferSize) => Console.OpenStandardInput(bufferSize);
+
+    public Stream OpenStandardOutput() => Console.OpenStandardOutput();
+
+    public Stream OpenStandardOutput(int bufferSize) => Console.OpenStandardOutput(bufferSize);
+
+    public Stream OpenStandardError() => Console.OpenStandardError();
+
+    public Stream OpenStandardError(int bufferSize) => Console.OpenStandardError(bufferSize);
+
+    public void SetIn(TextReader newIn) => Console.SetIn(newIn);
+
+    public void SetOut(TextWriter newOut) => Console.SetOut(newOut);
+
+    public void SetError(TextWriter newError) => Console.SetError(newError);
+
+    public int Read() => Console.Read();
+
+    public string? ReadLine() => Console.ReadLine();
+
     public void WriteLine() => Console.WriteLine();
+
+    public void WriteLine(bool value) => Console.WriteLine(value);
+
+    public void WriteLine(char value) => Console.WriteLine(value);
+
+    public void WriteLine(char[] buffer) => Console.WriteLine(buffer);
+
+    public void WriteLine(char[] buffer, int index, int count) => Console.WriteLine(buffer, index, count);
+
+    public void WriteLine(decimal value) => Console.WriteLine(value);
+
+    public void WriteLine(double value) => Console.WriteLine(value);
+
+    public void WriteLine(float value) => Console.WriteLine(value);
+
+    public void WriteLine(int value) => Console.WriteLine(value);
+
+    public void WriteLine(uint value) => Console.WriteLine(value);
+
+    public void WriteLine(long value) => Console.WriteLine(value);
+
+    public void WriteLine(ulong value) => Console.WriteLine(value);
+
+    public void WriteLine(object value) => Console.WriteLine(value);
+
+    public void WriteLine(string value) => Console.WriteLine(value);
 }
--- src/Adventure.Main/Adapters/Interfaces/IConsoleAdapter.cs
+++ src/Adventure.Main/Adapters/Interfaces/IConsoleAdapter.cs
@@ -18,20 +18,182 @@ public interface IConsoleAdapter
 
     #region Methods
 
-    /// <inheritdoc cref="Console.ReadKey(bool)"/> 
+    /// <inheritdoc cref="System.Console.WriteLine(System.String, System.Object)"/>
+    public void WriteLine(string format, object arg0);
+
+    /// <inheritdoc cref="System.Console.WriteLine(System.String, System.Object, System.Object)"/>
+    public void WriteLine(string format, object arg0, object arg1);
+
+    /// <inheritdoc cref="System.Console.WriteLine(System.String, System.Object, System.Object, System.Object)"/>
+    public void WriteLine(string format, object arg0, object arg1, object arg2);
+
+    /// <inheritdoc cref="System.Console.WriteLine(System.String, System.Object[])"/>
+    public void WriteLine(string format, object[] arg);
+
+    /// <inheritdoc cref="System.Console.Write(System.String, System.Object)"/>
+    public void Write(string format, object arg0);
+
+    /// <inheritdoc cref="System.Console.Write(System.String, System.Object, System.Object)"/>
+    public void Write(string format, object arg0, object arg1);
+
+    /// <inheritdoc cref="System.Console.Write(System.String, System.Object, System.Object, System.Object)"/>
+    public void Write(string format, object arg0, object arg1, object arg2);
+
+    /// <inheritdoc cref="System.Console.Write(System.String, System.Object[])"/>
+    public void Write(string format, object[] arg);
+
+    /// <inheritdoc cref="System.Console.Write(Boolean)"/>
+    public void Write(bool value);
+
+    /// <inheritdoc cref="System.Console.Write(Char)"/>
+    public void Write(char value);
+
+    /// <inheritdoc cref="System.Console.Write(Char[])"/>
+    public void Write(char[] buffer);
+
+    /// <inheritdoc cref="System.Console.Write(Char[], Int32, Int32)"/>
+    public void Write(char[] buffer, int index, int count);
+
+    /// <inheritdoc cref="System.Console.Write(Double)"/>
+    public void Write(double value);
+
+    /// <inheritdoc cref="System.Console.Write(System.Decimal)"/>
+    public void Write(decimal value);
+
+    /// <inheritdoc cref="System.Console.Write(Single)"/>
+    public void Write(float value);
+
+    /// <inheritdoc cref="System.Console.Write(Int32)"/>
+    public void Write(int value);
+
+    /// <inheritdoc cref="System.Console.Write(UInt32)"/>
+    public void Write(uint value);
+
+    /// <inheritdoc cref="System.Console.Write(Int64)"/>
+    public void Write(long value);
+
+    /// <inheritdoc cref="System.Console.Write(UInt64)"/>
+    public void Write(ulong value);
+
+    /// <inheritdoc cref="System.Console.Write(System.Object)"/>
+    public void Write(object value);
+
+    /// <inheritdoc cref="System.Console.Write(System.String)"/>
+    public void Write(string value);
+
+    /// <inheritdoc cref="System.Console.ReadKey()"/>
+    public ConsoleKeyInfo ReadKey();
+
+    /// <inheritdoc cref="System.Console.ReadKey(Boolean)"/>
     public ConsoleKeyInfo ReadKey(bool intercept);
 
-    /// <inheritdoc cref="Console.ResetColor()"/> 
+    /// <inheritdoc cref="System.Console.ResetColor()"/>
     public void ResetColor();
 
-    /// <inheritdoc cref="Console.SetCursorPosition(int, int)"/> 
+    /// <inheritdoc cref="System.Console.SetBufferSize(Int32, Int32)"/>
+    public void SetBufferSize(int width, int height);
+
+    /// <inheritdoc cref="System.Console.SetWindowPosition(Int32, Int32)"/>
+    public void SetWindowPosition(int left, int top);
+
+    /// <inheritdoc cref="System.Console.SetWindowSize(Int32, Int32)"/>
+    public void SetWindowSize(int width, int height);
+
+    /// <inheritdoc cref="System.Console.GetCursorPosition()"/>
+    public ValueTuple<int,int> GetCursorPosition();
+
+    /// <inheritdoc cref="System.Console.Beep()"/>
+    public void Beep();
+
+    /// <inheritdoc cref="System.Console.Beep(Int32, Int32)"/>
+    public void Beep(int frequency, int duration);
+
+    /// <inheritdoc cref="System.Console.MoveBufferArea(Int32, Int32, Int32, Int32, Int32, Int32)"/>
+    public void MoveBufferArea(int sourceLeft, int sourceTop, int sourceWidth, int sourceHeight, int targetLeft, int targetTop);
+
+    /// <inheritdoc cref="System.Console.MoveBufferArea(Int32, Int32, Int32, Int32, Int32, Int32, Char, System.ConsoleColor, System.ConsoleColor)"/>
+    public void MoveBufferArea(int sourceLeft, int sourceTop, int sourceWidth, int sourceHeight, int targetLeft, int targetTop, char sourceChar, ConsoleColor sourceForeColor, ConsoleColor sourceBackColor);
+
+    /// <inheritdoc cref="System.Console.Clear()"/>
+    public void Clear();
+
+    /// <inheritdoc cref="System.Console.SetCursorPosition(Int32, Int32)"/>
     public void SetCursorPosition(int left, int top);
 
-    /// <inheritdoc cref="Console.Write(string)"/> 
-    public void Write(string? value);
+    /// <inheritdoc cref="System.Console.OpenStandardInput()"/>
+    public Stream OpenStandardInput();
 
-    /// <inheritdoc cref="Console.WriteLine()"/> 
+    /// <inheritdoc cref="System.Console.OpenStandardInput(Int32)"/>
+    public Stream OpenStandardInput(int bufferSize);
+
+    /// <inheritdoc cref="System.Console.OpenStandardOutput()"/>
+    public Stream OpenStandardOutput();
+
+    /// <inheritdoc cref="System.Console.OpenStandardOutput(Int32)"/>
+    public Stream OpenStandardOutput(int bufferSize);
+
+    /// <inheritdoc cref="System.Console.OpenStandardError()"/>
+    public Stream OpenStandardError();
+
+    /// <inheritdoc cref="System.Console.OpenStandardError(Int32)"/>
+    public Stream OpenStandardError(int bufferSize);
+
+    /// <inheritdoc cref="System.Console.SetIn(System.IO.TextReader)"/>
+    public void SetIn(TextReader newIn);
+
+    /// <inheritdoc cref="System.Console.SetOut(System.IO.TextWriter)"/>
+    public void SetOut(TextWriter newOut);
+
+    /// <inheritdoc cref="System.Console.SetError(System.IO.TextWriter)"/>
+    public void SetError(TextWriter newError);
+
+    /// <inheritdoc cref="System.Console.Read()"/>
+    public int Read();
+
+    /// <inheritdoc cref="System.Console.ReadLine()"/>
+    public string? ReadLine();
+
+    /// <inheritdoc cref="System.Console.WriteLine()"/>
     public void WriteLine();
+
+    /// <inheritdoc cref="System.Console.WriteLine(Boolean)"/>
+    public void WriteLine(bool value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(Char)"/>
+    public void WriteLine(char value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(Char[])"/>
+    public void WriteLine(char[] buffer);
+
+    /// <inheritdoc cref="System.Console.WriteLine(Char[], Int32, Int32)"/>
+    public void WriteLine(char[] buffer, int index, int count);
+
+    /// <inheritdoc cref="System.Console.WriteLine(System.Decimal)"/>
+    public void WriteLine(decimal value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(Double)"/>
+    public void WriteLine(double value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(Single)"/>
+    public void WriteLine(float value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(Int32)"/>
+    public void WriteLine(int value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(UInt32)"/>
+    public void WriteLine(uint value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(Int64)"/>
+    public void WriteLine(long value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(UInt64)"/>
+    public void WriteLine(ulong value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(System.Object)"/>
+    public void WriteLine(object value);
+
+    /// <inheritdoc cref="System.Console.WriteLine(System.String)"/>
+    public void WriteLine(string value);
 
     #endregion
 }
